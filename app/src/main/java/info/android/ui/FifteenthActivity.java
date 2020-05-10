4b599664a6315b3fa7374eb4dff292b811ecc057
Vaibhav Grover <vaibhav.grover18@gmail.com>
2020-05-10 22:54:50 +0530

    Commit: FifteenthActivity - Keyframe Position Created; 10-05-2020 22-54

---
+++ app/src/main/java/info/android/ui/FifteenthActivity.java
@@ -0,0 +1,14 @@
+package info.android.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FifteenthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifteenth);
+    }
+}
