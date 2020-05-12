d8dbcfaf850b9a2f0f8c7dbbac9183bdef570694
Vaibhav Grover <vaibhav.grover18@gmail.com>
2020-05-12 23:29:56 +0530

    Commit: SeventeenthActivity - Keyframe Cycle Created; 12-05-2020 23-29

--- app/src/main/java/info/android/ui/MainActivity.java
+++ app/src/main/java/info/android/ui/MainActivity.java
@@ -37,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
         videos.add(new Video("Touch Controlled Motion with Changing Custom Attributes", R.raw.video_13, ThirteenthActivity.class));
         videos.add(new Video("Image Filter View Transition", R.raw.video_14, FourteenthActivity.class));
         videos.add(new Video("Keyframe Position", R.raw.video_15, FifteenthActivity.class));
-        videos.add(new Video("Keyframe Interpolation", R.raw.video_15, SixteenthActivity.class));
+        videos.add(new Video("Keyframe Interpolation", R.raw.video_16, SixteenthActivity.class));
+        videos.add(new Video("Keyframe Cycle", R.raw.video_17, SeventeenthActivity.class));
 
         RelativeLayout relativeLayout = findViewById(R.id.main_activity);
 
